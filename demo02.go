0b4192d1d45c096db4c841dd5ae16baa8825f65d
wangding <1@q.q>
2021-01-07 13:50:08 +0800

    .

--- demo02.go
+++ demo02.go
@@ -10,6 +10,18 @@ func main() {
 	// 1. var var1 string; var <变量名> <类型>
 	// 2. var var2 string = "string2"; var <变量名> <类型> = <值>
 	// 3. var var2 = "string2"; var <变量名> = <值>，根据值进行推断类型
-	var var2 = "string2"
-	fmt.Println(var2)
+	/*
+		4. var (
+			<变量名> <类型>
+			...
+		)
+	*/
+	/*
+		5. <变量名> := <值>
+	*/
+	/*
+		6. <变量名>... := <值>...
+	*/
+	var5, var6 := "test", "test1"
+	fmt.Println(var5, var6)
 }
