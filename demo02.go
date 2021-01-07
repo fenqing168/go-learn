fd7e70fd2dd575a8179f8b50fb8672c5bcab9da7
wangding <1@q.q>
2021-01-07 14:10:35 +0800

    .

--- demo02.go
+++ demo02.go
@@ -6,22 +6,24 @@ import (
 
 func main() {
 
-	// 变量声明
-	// 1. var var1 string; var <变量名> <类型>
-	// 2. var var2 string = "string2"; var <变量名> <类型> = <值>
-	// 3. var var2 = "string2"; var <变量名> = <值>，根据值进行推断类型
 	/*
+		1. var var1 string; var <变量名> <类型>
+		2. var var2 string = "string2"; var <变量名> <类型> = <值>
+		3. var var2 = "string2"; var <变量名> = <值>，根据值进行推断类型
 		4. var (
 			<变量名> <类型>
 			...
 		)
-	*/
-	/*
 		5. <变量名> := <值>
-	*/
-	/*
 		6. <变量名>... := <值>...
+		7. 指针变量 <变量名> = $<变量名>
+		8. *指针变量，取出指针对应的值，使用new函数创建的对象，返回的是变量地址
 	*/
-	var5, var6 := "test", "test1"
-	fmt.Println(var5, var6)
+	a, _ := GetData()
+	_, b := GetData()
+	fmt.Println(a, b)
+}
+
+func GetData() (int, int) {
+	return 100, 200
 }
