8766e2c4d04f3676afa3d73a9ae95bb4bb6567b6
wangding <1@q.q>
2021-01-07 11:38:26 +0800

    .

---
+++ demo02.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	// 变量声明
+	// 1. var var1 string; var <变量名> <类型>
+	// 2. var var2 string = "string2"; var <变量名> <类型> = <值>
+	// 3. var var2 = "string2"; var <变量名> = <值>，根据值进行推断类型
+	var var2 = "string2"
+	fmt.Println(var2)
+}
